80cccd4e81bf142db80e05a9f00296feb1f0a8da
Kamel BEN ABDELKADER <kamel.benabdelkader69@gmail.com>
2023-07-31 16:02:35 +0200

    adding azure script

---
+++ fast.py
@@ -0,0 +1,47 @@
+# 1. Library imports
+import uvicorn
+from typing import List
+# from models import Test,  TableBdd, Data
+from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
+import pymysql
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+import os
+import pickle
+import pandas as pd
+from Model import IrisModel, IrisSpecies
+
+# 2. Create the app object
+app = FastAPI()
+
+# 3. Index route, opens automatically on http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'message': 'Hello, stranger'}
+
+# # 4. Route with a single parameter, returns the parameter within a message
+# #    Located at: http://127.0.0.1:8000/AnyNameHere
+# @app.get('/{name}')
+# def get_name(name: str):
+#     return {'message': f'Hello, {name}'}
+
+
+# 5. Expose the prediction functionality, make a prediction from the passed
+#    JSON data and return the predicted flower species with the confidence
+@app.post('/predict')
+def predict_species(iris: IrisSpecies):
+    data = iris.dict()
+    prediction, probability = model.predict_species(
+        data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']
+    )
+    return {
+        'prediction': prediction,
+        'probability': probability
+    }
+
+
+# 4. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
