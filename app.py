c656255656faa0eb27f86789745d4aba0c15c540
Kamel BEN ABDELKADER <kamel.benabdelkader69@gmail.com>
2023-08-02 13:30:49 +0200

    CI/CD v9

--- app.py
+++ app.py
@@ -4,17 +4,41 @@ from typing import List
 from models import IrisSpecies ,  Prediction
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
-import pymysql
 from urllib.parse import urlparse
 from dotenv import load_dotenv
 import os
 import pickle
 import pandas as pd
 from Model import IrisModel
+import pymysql
 
+# Charger les variables d'environnement à partir du fichier .env
+load_dotenv()
 
 app = FastAPI()
 model = IrisModel()
+
+
+
+def connect():
+    # Récupérer l'URL de la base de données à partir des variables d'environnement
+    database_url = os.getenv("DATABASE_URL")
+
+    # Extraire les composants de l'URL de la base de données
+    url_components = urlparse(database_url)
+    db_host = url_components.hostname
+    db_user = url_components.username
+    db_password = url_components.password
+    db_name = url_components.path.strip('/')
+
+    # Configurer la connexion à la base de données MySQL
+    conn = pymysql.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_name
+    )
+    return conn
 
 # 3. Index route, opens automatically on http://127.0.0.1:8000
 @app.get('/')
@@ -35,6 +59,18 @@ def predict_species(iris: IrisSpecies):
         'probability': probability
     }
 
+
+
+@app.post("/add")
+async def create_item(item:  Prediction):
+    # Perform operations on the database
+    conn = connect()
+    with conn.cursor() as cursor:
+        query = "INSERT INTO prediction (prediction, probability) " \
+                 "VALUES (%s, %s,)"
+        values = (item.prediction, item.probability)
+        cursor.execute(query, values)
+        conn.commit()
 
 # 4. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
